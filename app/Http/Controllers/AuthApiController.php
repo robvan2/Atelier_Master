8746211683a08b95b9a256af95f1f70d106e4449
pc <mohamedkhalil1997@hotmail.fr>
2020-09-01 12:03:25 +0100

    added passport auth and Api routes

---
+++ app/Http/Controllers/AuthApiController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthApiController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'The email or password is incorrect, please try again'
+            ], 403);
+        }
+        $token = $user->createToken('access-token')->accessToken;
+
+        return response(['user' => $user, 'token' => $token], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+        $user = User::create(request(['name', 'email', 'password']));
+        $token = $user->createToken('access-token')->accessToken;
+        return response(['user' => $user, 'token' => $token], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response(['message' => 'user logged-out'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', 'AuthApiController@authenticate');
+Route::post('/register', 'AuthApiController@register');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/logout', 'AuthApiController@logout');
+
+    Route::post('/prediction/predict', 'FlaskController@sendImage');
+    Route::get('/test', 'FlaskController@predict');
+});
